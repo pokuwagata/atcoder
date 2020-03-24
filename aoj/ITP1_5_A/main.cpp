dfc368cafcdf26b5697216ea1da28c386578a11e
pokuwagata <pokuwagata@gmail.com>
2020-03-25 01:18:30 +0900

    day3

---
+++ aoj/ITP1_5_A/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+  while (1)
+  {
+    int h, w;
+    scanf("%d %d", &h, &w);
+    if(h == 0 && w == 0) break;
+    for (int i = 0; i < h; i++)
+    {
+      for (int j = 0; j < w; j++)
+      {
+        printf("#");
+      }
+      printf("\n");
+    }
+    printf("\n");
+  }
+  return 0;
+}
